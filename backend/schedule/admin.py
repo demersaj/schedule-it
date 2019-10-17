7a89a020a4c6e11c86cf3c148c54fe82a474ebfa
kmatusze13 <40547864+kmatusze13@users.noreply.github.com>
2019-10-17 14:46:45 -0500

    Update admin.py

--- backend/schedule/admin.py
+++ backend/schedule/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Student, Reservation, File, Slot
+from .models import Student, Professor, Reservation, File, Slot
 
 admin.site.register(Student)
+admin.site.register(Professor)
 admin.site.register(Reservation)
 admin.site.register(File)
 admin.site.register(Slot)
