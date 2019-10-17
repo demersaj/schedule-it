425460575542d56928b15f1b4e90fa38dac116ee
Andrew Demers <andrew.demers91@gmail.com>
2019-10-16 19:36:21 -0500

    Added calendar to project

---
+++ frontend/src/components/Calendar/Calendar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Calendar.css';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda,
+	EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+
+class Calendar extends React.Component {
+	 localData: EventSettingsModel = {
+		dataSource: [{
+			StartTime: new Date(2019, 9, 17, 14, 30),
+			EndTime: new Date(2019, 9, 17, 16, 30),
+			Subject: 'Work on CS 467 project'
+		}]
+	};
+
+	// allows you to load data from remote repository
+	 remoteData = new DataManager({
+		url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',    // url of api
+		adaptor: new WebApiAdaptor,
+		crossDomain: true
+	});
+
+	 render() {
+		return (
+			<ScheduleComponent
+				currentView={'Month'}
+				eventSettings={this.localData}>
+				<Inject services={ [Day, Week, WorkWeek, Month, Agenda] } />
+			</ScheduleComponent>
+		)
+	}
+}
+
+export default Calendar;
