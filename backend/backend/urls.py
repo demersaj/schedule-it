f738f47e14db29d5ba92e72cad4accf4e5d8d992
Andrew Demers <andrew.demers91@gmail.com>
2019-10-31 16:19:49 -0500

    Revert "Authentication works"
    This reverts commit eaaf3712224b859c08c3ef4924aa87b8ec61872a.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,5 +14,4 @@ router.register(r'slots', views.SlotView, 'Slot')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('auth/', include('rest_framework_social_oauth2.urls')),
 ]
--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -18,20 +18,23 @@ class Calendar extends Component {
 				'Max number of people': ''
 			},
 			isLoading: true
-		};
-		this.dataManager = new DataManager({
-			url: 'http://localhost:8000/api/slots/',    // url of api
-			adaptor: new JsonAdaptor(),
-			crossDomain: true
-		});
+		}
 	}
+
+
+	// allows you to load data from remote repository
+	remoteData = new DataManager({
+		url: 'http://localhost:8000/api/slots/',    // url of api
+		adaptor: new JsonAdaptor(),
+		crossDomain: true
+	});
 
 
 	render() {
 		return (
 			<ScheduleComponent
 				currentView={'Month'}
-				eventSettings={this.dataManager}
+				eventSettings={this.remoteData}
 				allowResizing={true}
 			>
 				<Inject services={ [Day, Week, WorkWeek, Month] } />
